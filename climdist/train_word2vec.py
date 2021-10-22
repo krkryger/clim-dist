3e81dc53f54f28c67ad384ad89da21e318c5384e
krkryger <krister.kruusmaa@hotmail.com>
2021-10-22 13:54:16 +0200

    Scripts for training embeddings

--- climdist/train_word2vec.py
+++ climdist/train_word2vec.py
@@ -1,21 +1,29 @@
+import sys
 import json
 import gensim.models
 import time
 import multiprocessing
 
+_sentences = str(sys.argv[1])
+_vector_size = sys.argv[2]
+_min_count = sys.argv[3]
+_epochs = sys.argv[4]
+
 class MyCorpus:
     """An iterator that yields sentences from the corpus"""
 
     def __iter__(self):
-        for line in open('../pipeline/rz_sentences.jsonl', 'r', encoding='utf8'):
+        for line in open(_sentences, 'r', encoding='utf8'):
             yield json.loads(line)
 
 print('Training Word2Vec')
 start = time.perf_counter()
 
-model = gensim.models.Word2Vec(vector_size=100,
-                                min_count=10,
-                                workers=multiprocessing.cpu_count())
+model = gensim.models.Word2Vec(sentences=MyCorpus(),
+                                vector_size=_vector_size,
+                                min_count=_min_count,
+                                workers=multiprocessing.cpu_count(),
+                                epochs=_epochs)
 
 stop = time.perf_counter()
 print(f'Finished in {round(stop-start)} seconds')
