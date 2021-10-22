02d2e9e8ca650f19e5857ece9c918be67143673e
krkryger <krister.kruusmaa@hotmail.com>
2021-10-22 12:45:04 +0300

    New sample sentences list for vectors

--- climdist/create_word_vectors.py
+++ climdist/create_word_vectors.py
@@ -1,4 +1,4 @@
-print('Importing modules')
+#print('Importing modules')
 
 import pandas as pd
 import spacy
@@ -7,28 +7,44 @@ import concurrent.futures
 import multiprocessing
 import json
 from tqdm import tqdm
+import string
 
-print('Loading spacy model')
+#print('Loading spacy model')
 nlp = spacy.load('de_core_news_md')
+
+
+def token_is_garbage(token, treshold=0.3):
+    
+    allowed_symbols = string.ascii_letters + '0123456789.äüö'
+    
+    non_alphabetical = 0
+    for symbol in token.text:
+        if symbol not in allowed_symbols:
+            non_alphabetical += 1
+            
+    if non_alphabetical/len(token.text) >= treshold:
+        return True
+    else:
+        return False
 
 
 def create_sentences_for_vec(df):
 
-    corpus = []
+    articles_as_lists = []
 
     for i in tqdm(df.index):
         text = df.loc[i, 'full_text']
         doc = nlp(text)
+        word_list = []
 
-        for sent in doc.sents:
-            sent_list = []
-            for word in sent:
-                if not word.is_stop:
-                    sent_list.append(word.text)
+        for token in doc:
+            if not token.is_stop and token.pos_ not in ['PUNCT', 'SPACE', 'NUM']:
+                if token_is_garbage(token) == False:
+                    word_list.append(token.text)
 
-        corpus.append(sent_list)
+        articles_as_lists.append(word_list)
 
-    return corpus
+    return articles_as_lists
 
 
 def divide_df(df, n):
@@ -43,13 +59,11 @@ if __name__ == '__main__':
     
     print(f'Starting main')
     start = time.perf_counter()
-    
     cpu_count = multiprocessing.cpu_count()
-    
-    all_sentences = []
+    all_articles = []
 
-    print('Importing df')
-    df = pd.read_parquet('C:/Users/krister/clim-dist/data/processed/RZ_processed.parquet')
+    #print('Importing df')
+    df = pd.read_parquet('C:/Users/krister/clim-dist/data/processed/RZ_sample.parquet')
     
     print('Processing')
     with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
@@ -57,10 +71,10 @@ if __name__ == '__main__':
         results = [executor.submit(create_sentences_for_vec, df) for df in frames]
 
         for f in concurrent.futures.as_completed(results):
-            all_sentences += f.result()
+            all_articles += f.result()
             
-    with open('C:/Users/krister/clim-dist/pipeline/RZ_sentences.json', 'w', encoding='utf8') as f:
-        json.dump(all_sentences, f)
+    with open('C:/Users/krister/clim-dist/pipeline/RZ_sentences_test.json', 'w', encoding='utf8') as f:
+        json.dump(all_articles, f)
             
 
     stop = time.perf_counter()
