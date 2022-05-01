9dfdac7d3708ce9bb921c47c96485fc75dd987e0
krkryger <krister.kruusmaa@hotmail.com>
2022-05-01 17:44:00 +0200

    Diachronic embeddings nb

--- climdist/train_word2vec.py
+++ climdist/train_word2vec.py
@@ -6,16 +6,25 @@ import json
 import gensim.models
 import time
 import multiprocessing
+from tqdm import tqdm
 
 
 class MyCorpus:
     """An iterator that yields sentences from the corpus"""
     def __iter__(self):
-        for line in open(_sentences, 'r', encoding='utf8'):
+        for line in tqdm(open(_sentences, 'r', encoding='utf8')):
             sentence = json.loads(line)
             for key, value in sentence.items():
                 if int(key) in indexes: # checks if the line is in the timerange
                     yield value
+
+
+class BigramIterator(): 
+    """Iterator for retrieving bigrammed sentences"""
+    def __iter__(self):
+        for sentence in MyCorpus():
+            yield bigram_model[sentence]
+
 
 if __name__ == '__main__':
 
@@ -25,6 +34,7 @@ if __name__ == '__main__':
     _min_count = int(sys.argv[4])    # 10
     _epochs = int(sys.argv[5])       # 10
     _timerange = range(int(sys.argv[6]), int(sys.argv[7]))
+    _bigram_path = str(sys.argv[8])
 
     if not os.path.exists('/'.join(_savepath.split('/')[:-1])):
         sys.exit(f'Savepath {_savepath} does not exist!')
@@ -32,10 +42,12 @@ if __name__ == '__main__':
     df = load('main', readability=True, heading2=False)
     indexes = df[(df.readability==True) & (df.year.isin(_timerange))].index
 
+    bigram_model = gensim.models.phrases.Phrases.load(_bigram_path)
+
     print('Training Word2Vec')
     start = time.perf_counter()
 
-    model = gensim.models.Word2Vec(sentences=MyCorpus(),
+    model = gensim.models.Word2Vec(sentences=BigramIterator(),
                                     vector_size=_vector_size,
                                     min_count=_min_count,
                                     workers=multiprocessing.cpu_count(),
