8c2354c2dec92e2356dfab0128304d90ddabccf1
krkryger <krister.kruusmaa@hotmail.com>
2022-05-14 22:50:09 +0200

    New heading2

--- climdist/data_preprocess.py
+++ climdist/data_preprocess.py
@@ -1,16 +1,22 @@
 import pandas as pd
 from tqdm import tqdm
+import re
+import json
+from tqdm import tqdm
+import numpy as np
 
-def basic_preprocess(output_path):
 
+def basic_preprocess():
     """Input: takes the two parquet files from data/raw, joins them, generates year, month date columns, checks for cuplicates.
         Output: main processed df for later use."""
 
     print('Importing raw data...')
 
-    df1 = pd.read_parquet('../../data/external/riga_zeit_1802_1859.parquet')
-    df2 = pd.read_parquet('../../data/external/riga_zeit_1859_1888.parquet')
+    df1 = pd.read_parquet('../data/external/riga_zeit_1802_1859.parquet')
+    df2 = pd.read_parquet('../data/external/riga_zeit_1859_1888.parquet')
     df = pd.concat([df1, df2])
+
+    print('Generating columns: year, month, day')
 
     # remove duplicate index column
     #df.drop(df.columns[0], axis=1, inplace=True)
@@ -20,6 +26,7 @@ def basic_preprocess(output_path):
     df['month'] = df['date'].str.slice(5, 7).astype(int)
     df['day'] = df['date'].str.slice(8, 10).astype(int)
     df = df[['date', 'year', 'month', 'day', 'pub', 'head', 'full_text', 'href']]
+    df.columns = ['date', 'year', 'month', 'day', 'pub', 'heading', 'full_text', 'href']
 
     print(f'There are {len(df)} entries in the dataset with {df.duplicated().sum()} duplicate rows.')
     print('Removing duplicates...')
@@ -30,98 +37,93 @@ def basic_preprocess(output_path):
     print('Calculating text lenghts in characters...')
     df['text_len'] = df.full_text.str.len()
 
-    # add a column with word counts
-    #print('Counting words...')
-    #df['w_count'] = df.full_text.str.split(' ')
-    #df['w_count'] = df.w_count.apply(len)
-
-    # normalize the most common headings
-    #print('Normalizing headings...')
-    #df = normalize_headings(df)
-
-    df = df.rename(columns={'head':'heading'})
-
-    if output_path:
-        # save the dataframe after changes that are applied
-        print('Saving the file...')
-        df.to_parquet(output_path, index=False)
-
     return df
 
 
-def normalize_headings(df):
+def get_places_and_dates(heading):
+    """Matches all the headings to a regex pattern that finds placenames
+    and dates in a certain common heading format"""
 
-    """Input: main df (after basic preprocess). Strips headings and uses a dict to manually converge some of the most widespread
-        variations in heading names.
-        Output: main df with normalized headings."""
-    
-    df['head'] = df['head'].str.strip('.')
-    df['head'] = df['head'].str.strip(',')
-    df['head'] = df['head'].str.strip(' ')
-    
-    df['head'] = df['head'].replace(to_replace=
-                {
-            'Witterungs-Beobachtungen in Riga': 'Witterungsbeobachtungen in Riga',
-            'Witterungs- Beobachtungen in Riga': 'Witterungsbeobachtungen in Riga',
-            'Meteorologische Beobachtungen in Riga': 'Witterungsbeobachtungen in Riga',
-            'Witterungs- Beobachtungen in Riga': 'Witterungsbeobachtungen in Riga',
-            'Witterungs -Beobachtungen in Riga': 'Witterungsbeobachtungen in Riga',
-            'Witterunsbeobachtungen in Riga': 'Witterungsbeobachtnogen in Riga',
-            'Groszbritannien und Irland': 'Großbritannien und Irland',
-            'Grossbritannien und Irland': 'Großbritannien und Irland',
-            'Grosbritannien und Irland': 'Großbritannien und Irland',
-            'Todes – Anzeige': 'Todes-Anzeige',
-            'Todes– Anzeige': 'Todes-Anzeige',
-            'Todes-Anzeigen': 'Todes-Anzeige',
-            'Todes – Anzeigen': 'Todes-Anzeige',
-            'Telegraphische Nachrichten': 'Telegramme',
-            'Lokales': 'Locales',
-            'Börsen- und Handels – Nachrichten': 'Börsen- und Handels-Nachrichten',
-            'Börsen – und Handels – Nachrichten': 'Börsen- und Handels-Nachrichten',
-            'Börsen und Handels-Nackrichten': 'Börsen- und Handels-Nachrichten',
-            'Börsen – und Handels – Nachrichten': 'Börsen- und Handels-Nachrichten',
-            'Telegraphische Depesche der „Rigaschen Zeitung"': 'Telegramme der „Rigaschen Zeitung"',
-            'Witterungs-Telegramme': 'Telegraphische Witterungsberichte',
-            'Vermischte Nachrichten': 'Vermischtes',
-            'Tägliche Eisenbahuzüge': 'Eisenbahnzüge',
-            'Tägliche – Eisenbahnzüge': 'Eisenbahnzüge',
-            'Telegr. Berichte über den Barometerstand': 'Telegraphische Witterungsberichte',
-            'Inländische Nachrichten': 'Inland',
-            'Inlandische Nachrichten': 'Inland',
-            'Telegr. der Rig. Telegraphen Agentur': 'Telegramme',
-            'Folgende Personen sind gesonnen, von hier zu reise': 'Abreisende',
-            'Nachstehende Personen zeigen ihre Abreise von hier': 'Abreisende',
-            'Tägliche – Eisenbahnzüge': 'Eisenbahnzüge',
-            'Deutsches «eich': 'Deutsches Reich',
-            'Neueste Rachrichten': 'Neueste Nachrichten',
-            'Neuste Nachrichten': 'Neueste Nachrichten',
-                }
-            )
-    
-    return df
+    re_pattern = '(?:Aus |AuS |Vom |Aus dem |(Schreiben aus ))?(?P<placename>(St. )?[A-Z][a-züöä]+)(?:( im )|(, im ))?(?P<gouv>(([A-Za-z][a-züöä]+chen Gou[a-z]+)|(Gou[a-züöä]+ )[A-Z][a-züöä]+)?)?(?:,|, |., |.,)(?:(den )|(vom ))(?P<date>\d{1,2})(?:\.|ten|sten)?'
+
+    match = re.search(re_pattern, heading)
+    if match:
+        return match.group('placename'), match.group('gouv'), match.group('date')
+    else:
+        return pd.NA
 
 
-def generate_alt_headings(df, ignorelocs, nlp,
-                          output_path=None):
+def apply_regex(df):
+    """Generates columns for placename and the date of the origin of the info"""
+    
+    print('Applying regex to search for placenames and dates')
+    patterns_in_headings = [get_places_and_dates(line) for line in df.heading]
+    
+    placenames = [entry[0] if type(entry) == tuple else entry for entry in patterns_in_headings]
+    #gouvernements = [entry[1] if type(entry) == tuple else entry for entry in patterns_in_headings]
+    dates = [entry[2] if type(entry) == tuple else entry for entry in patterns_in_headings]
+    
+    df['placename'] = placenames
+    #df['gouvernement'] = gouvernements
+    df['origin_date'] = dates
 
-    """Input: main df with normalized headings. Use a trained NLP model for detecting LOC entities in headings.
-        Output: column with alternate headings for simpler geographical analysis.
-        Can take 1-2 hours."""
-    
-    df['heading2'] = df['heading']
-    
-    for i in tqdm(df.index):
-        title = df.loc[i, 'heading2']
-        doc = nlp(title)
-        locs = [ent.text for ent in doc.ents if ent.label_ == 'LOC']
-        if len(locs) == 1: # add 2nd condition: if starts with loc!
-            if locs[0] not in ignorelocs:
-                df.loc[i, 'heading2'] = locs[0]
 
-    if output_path:
-        df['heading2'].to_parquet(output_path, index=True)
+def find_grenze(df):
+    """Handles an exception that is missed by the regex pattern"""
+
+    for ix, name in df.placename[df.placename.notna()].iteritems():
+        try:
+            if 'Gränze' in name:
+                heading_words = [word.strip('.,') for word in re.split('\s|,|-|.]', df.loc[ix, 'heading'])]
+                grenze_ix = heading_words.index('Gränze') 
+                new_placename = ' '.join(heading_words[grenze_ix-1:grenze_ix+1])
+                #print(new_placename)
+                df.loc[ix, 'placename'] = new_placename
+            elif 'Grenze' in name:
+                heading_words = [word.strip('.,') for word in re.split('\s|,|-|.]', df.loc[ix, 'heading'])]
+                grenze_ix = heading_words.index('Grenze') 
+                new_placename = ' '.join(heading_words[grenze_ix-1:grenze_ix+1])
+                #print(new_placename)
+                df.loc[ix, 'placename'] = new_placename
+            else:
+                pass
+        except:
+            pass
+                
     
-    return df['heading2']
+def remove_weekday_false_positives(df): 
+    """Handles an exception that is missed by the regex pattern"""
+    weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Sonntag', 'Sonnabend', 'Sonntag']
+    for name in weekdays:
+        df.placename.replace(name, pd.NA, inplace=True)
+
+
+def remove_false_dates(df):
+    """Handles an exception that is missed by the regex pattern"""
+    df.origin_date.fillna(0, inplace=True) 
+    df['origin_date'] = [date if int(date) in range(1,32) else pd.NA for date in df.origin_date]
+
+
+def generate_heading2(df):
+
+    apply_regex(df)
+    find_grenze(df)
+    remove_weekday_false_positives(df)
+    remove_false_dates(df)
+
+    with open('../pipeline/heading_replacement_dict.json', 'r', encoding='utf') as f:
+        replacement_dict = json.load(f)
+
+    df['heading2'] = df['heading'].copy()
+
+    print('Generating heading2...')
+    for key, value in tqdm(replacement_dict.items()):
+        df.heading2.replace(key, value, inplace=True)
+
+    df['heading2'] = df['heading2'].str.strip('., ')
+    df['heading2'].loc[df.placename.notna()] = df.placename
+    df.heading2.replace('Ist zu drucken erlaubt. Im Namen des General-Gouve', 'Ist zu drucken erlaubt...', inplace=True)
+    df = df.fillna(value=np.nan)
 
 
 def generate_ocr_column(df, output_path):
@@ -157,38 +159,19 @@ def generate_ocr_column(df, output_path):
     return column
 
 
-def complete_data_preprocess():
+def create_sample_df(df):
 
-    df = basic_preprocess(output_path='../../data/processed/RZ_processed.parquet')
-    df = normalize_headings(df)
-    
-    import spacy
-    
-    nlp = spacy.load('../data/models/spacy_model_250421/')
-    ignorelocs = ['Riga', 'Rigasche', 'Rigaschen', 'Rigische']
-    generate_alt_headings(df, ignorelocs=ignorelocs, nlp=nlp, output_path='../data/processed/RZ_heading2.parquet')
-
-    generate_ocr_column(df, output_path='../data/processed/OCR_readability_column.parquet')
-
-    print('Data preprocessing finished')
-
-
-def create_sample_df():
-
-    df = pd.read_parquet('../data/processed/RZ_processed.parquet')
-    df['readability'] = pd.read_parquet('../data/processed/RZ_readability.parquet')
-    df['heading2'] = pd.read_parquet('../data/processed/RZ_heading2.parquet')
-
-    sample_df = df[df.readability == 1].sample(10000)
-
+    sample_df = df.sample(10000)
     sample_df.to_parquet('../data/processed/RZ_sample.parquet')
-
-    print('Sample ready!')
-
+    print('Created sample')
 
 
-#if __name__ == "__main__":
-#
-#   complete_data_preprocess()
+if __name__ == "__main__":
+
+    df = basic_preprocess()
+    generate_heading2(df)
+    create_sample_df(df)
+    df.to_parquet('../data/processed/RZ_processed.parquet')
+    print('Finished')
 
     
---
+++ climdist/utils.py
@@ -0,0 +1,205 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+from tqdm.notebook import tqdm
+from wordcloud import WordCloud
+import matplotlib.font_manager as fm
+
+
+def load_df(which):
+
+    """Load the Rigasche Zeitung data.
+        which: str, 'main', 'sample'
+        readability: bool, include the OCR readability column
+        heading2: bool, include the heading2 column"""
+
+    if which == 'main':
+        df = pd.read_parquet('../data/processed/RZ_processed.parquet')
+        df['readability'] = pd.read_parquet('../data/processed/RZ_readability.parquet')
+
+
+        #if readability == True:
+        #if heading2 == True:
+            #df['heading2'] = pd.read_parquet('../data/processed/RZ_heading2.parquet')
+
+    elif which == 'sample':
+        df = pd.read_parquet('../data/processed/RZ_sample.parquet')
+
+    return df
+
+
+class Analyse():
+    
+    def __init__(self):
+        
+        print('Importing main dataframe')
+        self.df = load_df('main')
+        self.df = self.df[self.df.readability==True]
+        
+        print('Importing processed articles')
+        self.sentences = list(self.load_sentences())
+        
+        print('Importing detected keywords')
+        with open('../data/processed/all_keywords_100522.json', 'r', encoding='utf') as f:
+            self.keywords = json.load(f)
+            
+        print('Importing auxiliaries')
+        self.ruler = pd.read_excel('../pipeline/ner/ruler_patterns_060522.xlsx', index_col='Unnamed: 0')
+        self.wea_df = pd.read_csv('../data/processed/wea_df_010522.tsv', sep='\t', encoding='utf8', index_col=0)
+        self.wordform_df = pd.read_csv('../data/processed/wordform_df_010522.tsv', sep='\t', encoding='utf8', index_col=0)
+        self.phenomenon_df = pd.read_csv('../data/processed/phenomenon_df_110522.tsv', sep='\t', encoding='utf8', index_col=0)
+        
+        self.ix_to_year = self.df['year'].to_dict()
+            
+        self.font_path = '../references/cmunorm.ttf'
+        fm.fontManager.addfont(self.font_path)
+
+        print('Ready!')
+        
+    def wordform_to_key(self, wordform):
+        return list(self.ruler.key[self.ruler.wordform == wordform].unique())
+    
+    def phenomenon_to_key(self, phenomenon):
+        return list(self.ruler.key[self.ruler.phenomenon == phenomenon].unique())
+    
+    def phenomenon_to_wordform(self, phenomenon):
+        return list(self.ruler.wordform[self.ruler.phenomenon == phenomenon].unique())
+        
+    def load_sentences(self):
+        with open('../data/processed/RZ_sentences.jsonl', 'r', encoding='utf8') as f:
+            for ix, line in tqdm(enumerate(f.readlines())):
+                yield json.loads(line)
+                
+                
+    def ids_by_kw(self, keys, timerange, index_filter=None): 
+        """Returns indexes of all entites that contain a certain keyword"""
+    
+        if index_filter is None:
+            index_filter = self.df.index
+        keys=set(keys)
+        results = []
+        
+        for sentence, entry in zip(self.sentences, self.keywords):
+            if sentence['id'] in index_filter \
+            and len(entry['ents']) > 0 \
+            and self.df.loc[sentence['id'], 'year'] in timerange \
+            and len(set([ent[0] for ent in entry['ents']]) & keys) > 0: 
+                    results.append(sentence['id'])
+                    
+        return results
+    
+    
+    def headings_by_kw(self, words, timerange=range(1802,1889), phenomenon=False): 
+        """Returns a pd.Series.value_counts() object of headings of articles that contain a given keyword"""
+    
+        if phenomenon == True:
+            keys = set(self.ruler.key[self.ruler.phenomenon.isin(words)])
+        else:
+            keys = set(self.ruler.key[self.ruler.wordform.isin(words)])    
+        ids = self.ids_by_kw(keys, timerange)
+        
+        return self.df.heading2.loc[ids].value_counts()
+    
+    
+    def word_context(self, word, window, index_filter=None, phenomenon=False):
+        """Input: wordform. Returns a list of context words in a defined window"""
+    
+        if index_filter is None:
+            index_filter = self.df.index
+        if phenomenon == True:
+            keys = self.phenomenon_to_key(word)
+        else:
+            keys = self.wordform_to_key(word)
+            
+        results = []
+        
+        for sentence, entry in tqdm(zip(self.sentences, self.keywords)):
+            if sentence['id'] in index_filter:
+                if len(entry['ents']) > 0:
+                    for ent in entry['ents']:
+                        if ent[0] in keys:
+                            ix = ent[1]
+                            context_before = sentence['text'][ix-window:ix]
+                            context_after  = sentence['text'][ix+1:ix+window]
+                            results.extend(context_before + context_after)
+           
+        return results
+    
+    
+    def keyword_plot(self, keywords, cat='wordform', kind='line', wordform=True, relative=True, savepath=None, **kwargs):
+    
+        if cat == 'key':
+            plot_df = self.wea_df
+        elif cat == 'wordform':
+            plot_df = self.wordform_df
+        elif cat == 'phenomenon':
+            plot_df = self.phenomenon_df
+        else:
+            return('Please select a valid CAT')
+        
+        matplotlib.rcParams['font.family'] = 'CMU Concrete'
+        col = sns.color_palette("muted", len(keywords))
+        plt.figure(figsize=(15,7))
+        ticksrange = np.arange(1802,1890)
+        xlabels = [num if num%5==0 else '' for num in np.arange(1802,1890)]
+        
+        ### lineplots
+        if kind == 'line':
+            if relative:
+                for word in keywords:
+                    plt.plot(plot_df[word]/plot_df.total, label=word)             
+            else:
+                for word in keywords:
+                    plt.plot(plot_df[word], label=word)
+                
+        ### stackplots            
+        if kind == 'stack':
+            if relative == True:
+                plt.stackplot(np.arange(1802,1889),
+                              [plot_df[word]/plot_df.total for word in keywords],
+                              labels=keywords,
+                              colors=col)
+            else:
+                plt.stackplot(np.arange(1802,1889), [plot_df[word] for word in keywords], labels=keywords)    
+        
+        plt.grid(b=True, which='both')
+        plt.yticks(fontsize=14)
+        plt.xticks(ticks=ticksrange, labels=xlabels, fontsize=14)
+        plt.tick_params(axis ='x', rotation = 45)
+        plt.setp(plt.gca().spines.values(), color='black')
+        plt.gca().set_frame_on(True)
+        
+        handles, labels = plt.gca().get_legend_handles_labels()
+        plt.legend(handles[::-1], labels[::-1], fontsize=12, **kwargs)
+    
+        if relative == True:
+            plt.ylabel("part de tous les mots-clés", fontsize=18)
+        else:
+            plt.ylabel("nb. d'occurrences", fontsize=18)
+            
+        sns.set_style(style='whitegrid')
+        
+        if savepath:
+            plt.savefig(savepath, bbox_inches='tight')
+        
+        plt.show()
+        
+        
+    def context_wordcloud(self, context_words, savepath=None):
+    
+        wordcloud_data = ' '.join([word for word in context_words])
+        
+        print(f'Generated from {len(wordcloud_data.split())} context words')
+        wc = WordCloud(background_color='white', width=900, height=300, font_path='../references/cmunrm.ttf')
+        wc.generate(wordcloud_data)
+        plt.figure(figsize=(12,4))
+        plt.axis('off')
+        
+        plt.imshow(wc)
+        if savepath:
+            plt.savefig(savepath, bbox_inches='tight')
+        plt.show()
+                    
+    
